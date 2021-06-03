908b9cfba002f7ab78f68a358fe2ea90377a344d
Mitchell Rysavy <mrysav@github.com>
2021-06-03 13:52:31 -0400

    mac bash completions

--- mac.bash
+++ mac.bash
@@ -1,4 +1,8 @@
+# One part bash_profile, one part bashrc... it's the mac customizations
+
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
 
 export PS1="\[\033[1;32m\]@\u:\033[34m\w\033[32m$\\[\033[0m\] "
+
+  [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
