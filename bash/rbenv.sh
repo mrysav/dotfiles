4eaa96f544806014c886d9c395428353cc910d0b
Mitchell Rysavy <mrysav@github.com>
2021-08-23 15:31:43 -0400

    Fix the order of things I hope

--- bash/rbenv.sh
+++ bash/rbenv.sh
@@ -1,8 +1,8 @@
 # shellcheck shell=bash
 
+if [[ -d "$HOME/.rbenv/bin" ]]; then
+    export PATH="$HOME/.rbenv/bin:$PATH"
+fi
 if [[ -z "$(which rbenv)" ]]; then
-    if [[ -d "$HOME/.rbenv/bin" ]]; then
-        export PATH="$HOME/.rbenv/bin:$PATH"
-    fi
     eval "$(rbenv init --no-rehash -)"
 fi
