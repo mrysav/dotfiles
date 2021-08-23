3a1c3000470b96f7a3b95a748f73073cf327f881
Mitchell Rysavy <mrysav@github.com>
2021-08-23 15:38:05 -0400

    omg just make it work

--- bash/rbenv.sh
+++ bash/rbenv.sh
@@ -3,6 +3,6 @@
 if [[ -d "$HOME/.rbenv/bin" ]]; then
     export PATH="$HOME/.rbenv/bin:$PATH"
 fi
-if [[ -z "$(which rbenv)" ]]; then
+if which rbenv>/dev/null; then
     eval "$(rbenv init --no-rehash -)"
 fi
