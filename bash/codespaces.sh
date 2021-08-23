6db9e7c268e6b66e07d3c2156f9f72151dd021bd
Mitchell Rysavy <mrysav@github.com>
2021-08-23 15:43:25 -0400

    linuxbrew eager load

--- bash/codespaces.sh
+++ bash/codespaces.sh
@@ -2,3 +2,7 @@
 
 # For codespaces config, specifically...
 
+# Eager load linux brew if it's there. It's just easier that way.
+if [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
