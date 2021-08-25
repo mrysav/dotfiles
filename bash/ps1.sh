94d9e6c56a346aff2b9f53279d220f7d5a72633b
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2021-08-24 21:45:18 -0400

    ps1 fix for devcontainers

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -12,6 +12,11 @@ bold_blue="\e[1;34m"
 if [[ "$CODESPACES" == "true" && -n $CODESPACE_NAME ]]; then
     PSUSR="$(echo "$CODESPACE_NAME" | sed 's/\-/ /g' | awk '{ print $1 }')"
     PSHOST=""
+elif [[ -z "$REMOTE_CONTAINERS_IPC" ]]; then
+    # This is probably not foolproof, but if you're using a vscode devcontainer,
+    # (not on codespaces) this will probably work fine?
+    PSUSR="\u"
+    PSHOST=""
 else
     PSUSR="\u"
     PSHOST="@\h"
