7598c734e63e3fe825ba5fb959e7050578d3e061
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2021-08-24 21:46:43 -0400

    one of these days I will remember -n and -z

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -12,7 +12,7 @@ bold_blue="\e[1;34m"
 if [[ "$CODESPACES" == "true" && -n $CODESPACE_NAME ]]; then
     PSUSR="$(echo "$CODESPACE_NAME" | sed 's/\-/ /g' | awk '{ print $1 }')"
     PSHOST=""
-elif [[ -z "$REMOTE_CONTAINERS_IPC" ]]; then
+elif [[ -n "$REMOTE_CONTAINERS_IPC" ]]; then
     # This is probably not foolproof, but if you're using a vscode devcontainer,
     # (not on codespaces) this will probably work fine?
     PSUSR="\u"
