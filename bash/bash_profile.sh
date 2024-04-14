143a6f981e3fbaa877db2ac1929088fa5fdbdc0d
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2024-04-14 19:57:33 -0400

    fix tmux and bash_profile

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -1,5 +1,13 @@
 # shellcheck shell=bash
 
+if [[ -n "$TMUX" ]]; then
+   # Kinda weird but tmux doesn't start an interactive shell by default I guess
+   # so instead of changing tmux config, just make sure to resource this
+
+   # shellcheck disable=SC1091
+   source "$HOME/.bashrc"
+fi
+
 if which ssh-agent >/dev/null && [ -z "$SSH_AUTH_SOCK" ]; then
    # Check for a currently running instance of the agent
    RUNNING_AGENT="$(ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]')"
