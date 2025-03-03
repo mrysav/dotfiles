b9ee0fbb09feea4d92270a1d368c6c1c01ebdc2a
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2025-03-02 22:53:18 -0500

    Fix pyenv

--- bash/pyenv.sh
+++ bash/pyenv.sh
@@ -1,17 +1,16 @@
 # shellcheck shell=bash
 
-# Guard against init'ing twice
-if ! [[ $PATH == *"pyenv/shims"* ]]; then
+if which pyenv >/dev/null 2>/dev/null; then
+    PYENV_BINDIR="$(dirname "$(which pyenv)")"
+    PYENV_ROOT="$(dirname "$PYENV_BINDIR")"
+    export PYENV_ROOT
 
-    if [[ -d "$HOME/.pyenv/bin" ]]; then
-        export PATH="$HOME/.pyenv/bin:$PATH"
+    if ! [[ $PATH == *"pyenv/shims"* ]]; then
+        export PATH="$PYENV_ROOT/bin:$PATH"
     fi
 
-    if which pyenv >/dev/null; then
-        eval "$(pyenv init --path)"
-        eval "$(pyenv init -)"
-        eval "$(pyenv virtualenv-init -)"
-    fi
+    eval "$(pyenv init - bash)"
+    # eval "$(pyenv virtualenv-init -)"
 fi
 
 
